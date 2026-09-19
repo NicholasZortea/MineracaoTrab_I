3fc2132d525a3345f4a56c5cc7f9c7976c6d3a2b
Nicholas Zortea Graczik <zorteanicholas619@gmail.com>
2026-09-19 11:50:09 -0300

    Feat: Create separate columns for win and defeat

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from mlxtend.frequent_patterns import apriori, association_rules
 
 df = pd.read_csv('_ASSOC_VoleiStars.csv', encoding='latin1')
 
@@ -16,7 +17,6 @@ mapa_nomes = {
     'ana': 'Ana'
 }
 
-
 def limpar_jogadores(texto):
 
     jogadores = [j.strip() for j in texto.split(',')]
@@ -31,8 +31,32 @@ def limpar_jogadores(texto):
         
     return ', '.join(jogadores_limpos)
 
+def df_one_hot_encoding(df):
 
+    colunas = nomes_unicos(df)
+    colunas += ["Vitória", "Derrota"]
+    df_ohe = pd.DataFrame(columns=colunas)
 
+    for i in range(len(df)):
+
+        linha_ohe = {nome: 0 for nome in colunas}
+
+        nomes = df.loc[i, 'Jogadore(a)s'].split(',')
+
+        for nome in nomes:
+            nome_limpo = nome.strip()
+            if nome_limpo in linha_ohe:
+                linha_ohe[nome_limpo] = 1
+
+        resultado = df.loc[i, "Resultado"].lower()
+        if resultado == "ganhou":
+            linha_ohe["Vitória"] = 1
+        else :
+            linha_ohe["Derrota"] = 1
+
+        df_ohe = pd.concat([df_ohe, pd.DataFrame([linha_ohe])], ignore_index=True)
+
+    return df_ohe
 
 def nomes_unicos(df):
     nomes_unicos = []
@@ -48,37 +72,20 @@ def nomes_unicos(df):
 
     return nomes_unicos
 
-
-
-def df_one_hot_encoding(df):
-
-    colunas = nomes_unicos(df)
-    df_ohe = pd.DataFrame(columns=colunas)  
-
-    for i in df['Jogadore(a)s']:
-
-        linha_ohe = {nome: 0 for nome in colunas}   
-
-        nomes = i.split(',')
-
-
-        for nome in nomes:
-            nome_limpo = nome.strip()
-            if nome_limpo in linha_ohe:
-                linha_ohe[nome_limpo] = 1
-
-        df_ohe = pd.concat([df_ohe, pd.DataFrame([linha_ohe])], ignore_index=True)
-        df_ohe['Resultado']= df['Resultado']
-
-        df_ohe['Resultado'] = df_ohe['Resultado'].map({'GANHOU': 1}).fillna(0).astype(int)
-
-
-    return df_ohe    
-
-    
-
-
 df_limpo = df.drop(columns=['Jogadore(a)s.1'])
 df_limpo['Jogadore(a)s'] = df['Jogadore(a)s'].apply(limpar_jogadores)
 df_ohe = df_one_hot_encoding(df_limpo)
 print(df_ohe)
+
+frequent_itemsets = apriori(
+    df_ohe,
+    min_support=0.05,
+    use_colnames=True
+)
+
+rules = association_rules(
+    frequent_itemsets,
+    metric="confidence",
+    min_threshold=0.5
+)
+print(rules)
